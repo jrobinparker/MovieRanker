c5557498bd44dcb6cc925dcef65c2889367f0ed1
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2017-07-24 14:20:51 -0700

    Completed MovieRanker site

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724035244) do
+ActiveRecord::Schema.define(version: 20170724194927) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "cached_votes_total", default: 0
+    t.integer "cached_votes_score", default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "cached_votes_down", default: 0
+    t.integer "cached_weighted_score", default: 0
+    t.integer "cached_weighted_total", default: 0
+    t.float "cached_weighted_average", default: 0.0
+    t.index ["cached_votes_down"], name: "index_movies_on_cached_votes_down"
+    t.index ["cached_votes_score"], name: "index_movies_on_cached_votes_score"
+    t.index ["cached_votes_total"], name: "index_movies_on_cached_votes_total"
+    t.index ["cached_votes_up"], name: "index_movies_on_cached_votes_up"
+    t.index ["cached_weighted_average"], name: "index_movies_on_cached_weighted_average"
+    t.index ["cached_weighted_score"], name: "index_movies_on_cached_weighted_score"
+    t.index ["cached_weighted_total"], name: "index_movies_on_cached_weighted_total"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,6 +48,22 @@ ActiveRecord::Schema.define(version: 20170724035244) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.string "votable_type"
+    t.integer "votable_id"
+    t.string "voter_type"
+    t.integer "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
 end
